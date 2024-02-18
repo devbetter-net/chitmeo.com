9768ce5d32f8bf32f1d9b197a13258b76e12cb9d
kyle <chitmeo@chitmeo.com>
2024-02-18 11:24:22 +0700

    Add new components and templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'chitmeo';
-}
+  }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: '', component: LayoutComponent,
+        children: [
+            { path: 'home', component: HomeComponent },
+        ]
+    },
+];
